d1c91b418e177a640d2af33a18087e660e2906a2
Robin Seidel <robin.seidel@tum.de>
2022-01-12 17:22:28 +0100

    docs: Added example images

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from distutils.core import setup
-setup(
-  name = 'pyrelvis',
-  packages = ['pyrelvis'], 
-  version = '0.1',
-  license='MIT',
-  description = 'Pyrelvis is a simple binary relation visualizer',
-  author = 'Robin Seidel',
-  author_email = 'robin.seidel@tum.de',
-  url = 'https://github.com/RobinSeidel/pyrelvis',
-  download_url = 'https://github.com/RobinSeidel/pyrelvis/archive/refs/tags/v0.0.1-alpha.tar.gz'
-  keywords = ['relation', 'binary', 'visualizer', 'svg'],
-  install_requires=[
-          'pycairo',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3', 
-  ],
 
+setup(
+    name="pyrelvis",
+    packages=["pyrelvis"],
+    version="0.0.1",
+    license="MIT",
+    description="Pyrelvis is a simple binary relation visualizer",
+    author="Robin Seidel",
+    author_email="robin.seidel@tum.de",
+    url="https://github.com/RobinSeidel/pyrelvis",
+    download_url="https://github.com/RobinSeidel/pyrelvis/archive/refs/tags/v0.0.1-alpha.tar.gz",
+    keywords=["relation", "binary", "visualizer", "svg"],
+    install_requires=[
+        "pycairo",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
